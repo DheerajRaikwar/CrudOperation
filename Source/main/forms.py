e24c01e64fe23e154dcf6b8e4d647456070e2dd6
DheerajRaikwar <54433649+DheerajRaikwar@users.noreply.github.com>
2021-02-13 17:19:01 +0530

    Changes for index page

--- Source/main/forms.py
+++ Source/main/forms.py
@@ -5,3 +5,10 @@ class StudentRegiser(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name','email','Dob','city','password']
+        widgets ={
+            'name':forms.TextInput(attrs={'class':'form-control','id':'nameid'}),
+            'email':forms.TextInput(attrs={'class':'form-control','id':'emailid'}),
+            'city':forms.TextInput(attrs={'class':'form-control','id':'cityid'}),
+            'Dob':forms.TextInput(attrs={'class':'form-control','id':'dobid'}),
+            'password':forms.TextInput(attrs={'class':'form-control','id':'passwordid'}),
+        }
--- Source/main/views.py
+++ Source/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .forms import StudentRegiser
+from .models import User
 # Create your views here.
 def Homepage(request):
-    return render(request,'Base.html')
+    form = StudentRegiser()
+    stud = User.objects.all()
+    return render(request,'index.html',{'form':form,'stud':stud})
