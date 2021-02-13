f36bd49d65b032db36eee22051bcb9dae2318554
DheerajRaikwar <54433649+DheerajRaikwar@users.noreply.github.com>
2021-02-14 00:33:38 +0530

    Operations Done for Insertion using Ajax and JSON

--- Source/main/views.py
+++ Source/main/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from .forms import StudentRegiser
 from .models import User
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def Homepage(request):
     form = StudentRegiser()
     stud = User.objects.all()
     return render(request,'index.html',{'form':form,'stud':stud})
 
-
+@csrf_exempt
 def save_file(request):
-    if render.method == "POST":
+    if request.method == "POST":
         form = StudentRegiser(request.POST)
         if form.is_valid():
             name = request.POST['name']
             email = request.POST['email']
-            dob = request.POST['dob']
+            dob = request.POST['Dob']
             city = request.POST['city']
             password = request.POST['password']
-            user =User(name=)
+            user = User(name=name,email=email,Dob=dob,city=city,password=password)
+            user.save()
+            return JsonResponse({'status':'Save'})
+        else:
+            return JsonResponse({'status':0})
